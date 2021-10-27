6eae77e17ad00480f5f630b2920e9160fce5f514
Stephen Bothwell <bothwells.xu@gmail.com>
2021-10-27 18:30:33 -0400

    Created new branch. Added base files for new julia_mt implementations.

---
+++ julia_mt/kmeans/kmeans.jl
@@ -0,0 +1,6 @@
+#!/usr/bin/env julia
+import Base.Threads
+
+# TODO: there isn't a single-threaded Julia implementation of this one, so it would require more work.
+# Thus, it might be better to start with the other two and advance to this one.
+# That, or use Rodinia (and other implementations) to guide the work.
